94085a6d2a1cbe4b56879960f91b4ccdaa9af6bc
Erik Torres <erik.torres@lifullconnect.com>
2022-05-03 09:32:10 +0200

    :white_check_mark: Better test cases generators

--- src/test/scala/acceptance/MarkdownTransformationUseCaseAcceptanceTest.scala
+++ src/test/scala/acceptance/MarkdownTransformationUseCaseAcceptanceTest.scala
@@ -2,11 +2,11 @@ package es.eriktorr.markdown_transformation
 package acceptance
 
 import acceptance.MarkdownTransformationUseCaseAcceptanceTest.TestCase
-import infrastructure.MarkdownGenerators.{lineGen, linkGen}
+import infrastructure.MarkdownGenerators.Fragment.{LinkFragment, TextFragment}
+import infrastructure.MarkdownGenerators.{fragmentsGen, linkGen, Fragment}
 import model.{Footnote, Line, Link, Reference}
 
 import cats.effect.IO
-import es.eriktorr.markdown_transformation.FakeMarkdownTransformationResources.withResources
 import munit.{CatsEffectSuite, ScalaCheckEffectSuite}
 import org.scalacheck.Gen
 import org.scalacheck.effect.PropF.forAllF
@@ -17,18 +17,46 @@ final class MarkdownTransformationUseCaseAcceptanceTest
 
   test("it should transform a markdown document") {
     val tesCaseGen = for
-      links <- Gen.containerOf[Set, Link](linkGen).map(_.toList)
-      footnotes = links.zipWithIndex.map { case (link, index) =>
-        Footnote(Reference(index + 1), link)
-      }
+      links <- Gen.nonEmptyContainerOf[Set, Link](linkGen).map(_.toList)
       numberOfLines <- Gen.choose(0, 5)
-      lines <- Gen.listOfN[Line](numberOfLines, lineGen(links))
-      initialState = MarkdownTransformationState.empty.setReaderLines(lines)
-      expectedState = initialState.setFootnotes(footnotes).setWriterLines(lines)
+      lineFragments <- Gen.listOfN[List[Fragment]](numberOfLines, fragmentsGen(links))
+      footnotes = lineFragments
+        .flatMap(xs => xs.collect { case LinkFragment(link) => link })
+        .toSet
+        .zipWithIndex
+        .map { case (link, index) => Footnote(Reference(index + 1), link) }
+        .toList
+        .sorted
+      initialState = MarkdownTransformationState.empty.setReaderLines(
+        lineFragments
+          .map(_.map {
+            _ match
+              case LinkFragment(link) => s"[${link.text}](${link.url})"
+              case TextFragment(text) => text
+          })
+          .map(_.mkString(" "))
+          .map(Line(_)),
+      )
+      expectedState = initialState
+        .setFootnotes(footnotes)
+        .setWriterLines(
+          (lineFragments
+            .map(_.map {
+              _ match
+                case LinkFragment(link) =>
+                  footnotes
+                    .find(_.link == link)
+                    .fold("")(x => s"${x.link.text} [^${x.reference.value}]")
+                case TextFragment(text) => text
+            })
+            .map(_.mkString(" ")) ++ footnotes
+            .map(x => s"[^${x.reference.value}]: ${x.link.url}")).map(Line(_)),
+        )
     yield TestCase(initialState, expectedState)
 
     forAllF(tesCaseGen) { testCase =>
-      withResources(testCase.initialState)(_.markdownTransformationUseCase.run)
+      FakeMarkdownTransformationResources
+        .withResources(testCase.initialState)(_.markdownTransformationUseCase.run)
         .map { case (result, finalState) =>
           assert(result.isRight)
           assertEquals(finalState, testCase.expectedState)
--- src/test/scala/infrastructure/MarkdownGenerators.scala
+++ src/test/scala/infrastructure/MarkdownGenerators.scala
@@ -1,6 +1,7 @@
 package es.eriktorr.markdown_transformation
 package infrastructure
 
+import infrastructure.MarkdownGenerators.Fragment.{LinkFragment, TextFragment}
 import model.{Line, Link, LinkText, LinkUrl}
 
 import org.scalacheck.Gen
@@ -14,34 +15,39 @@ object MarkdownGenerators:
       linkUrl <- urlGen
     yield Link(LinkText(linkText), LinkUrl(linkUrl))
 
-  def textGen(minLength: Int = 3, maxLength: Int = 10): Gen[String] = for
-    length <- Gen.choose(minLength, maxLength)
-    text <- Gen.listOfN[Char](length, Gen.alphaNumChar).map(_.mkString)
-  yield text
-
-  def lineGen(linksGen: Gen[List[Link]] = Gen.listOf[Link](linkGen)): Gen[Line] = for
-    numberOfFragments <- Gen.choose(0, 10)
-    fragments <- Gen.listOfN[String](numberOfFragments, textGen())
-    links <- linksGen.map(_.map { case Link(linkText, linkUrl) => s"[$linkText]($linkUrl)" })
-    content = Random.shuffle(fragments ++ links).mkString(" ")
-  yield Line(content)
-
-  def httpTypeGen: Gen[String] = Gen.oneOf(Seq("http", "https"))
-  def domainGen: Gen[String] = for
-    numberOfFragments <- Gen.choose(1, 3)
-    domain <- Gen.listOfN[String](numberOfFragments, textGen()).map(_.mkString("."))
-  yield domain
-  def domainTypeGen: Gen[String] = Gen.oneOf(Seq("com", "es", "org"))
-  def pathGen: Gen[String] = for
-    numberOfFragments <- Gen.choose(1, 3)
-    path <- Gen.listOfN[String](numberOfFragments, textGen()).map(_.mkString("/"))
-  yield path
+  def fragmentsGen(links: List[Link]): Gen[List[Fragment]] = for
+    numberOfTexts <- Gen.choose(0, 12)
+    textFragments <- Gen.listOfN[String](numberOfTexts, textGen()).map(_.map(TextFragment(_)))
+    numberOfLinks <- Gen.choose(0, 4)
+    linkFragments <- Gen.listOfN[Link](numberOfLinks, Gen.oneOf(links)).map(_.map(LinkFragment(_)))
+  yield Random.shuffle(textFragments ++ linkFragments)
 
   /** https://medium.com/@supermanue/building-useful-scalacheck-generators-71635d1edb9d */
-  def urlGen: Gen[String] =
+  private[this] def urlGen: Gen[String] =
+    def httpTypeGen: Gen[String] = Gen.oneOf(Seq("http", "https"))
+    def domainGen: Gen[String] = for
+      numberOfFragments <- Gen.choose(1, 3)
+      domain <- Gen.listOfN[String](numberOfFragments, textGen()).map(_.mkString("."))
+    yield domain
+    def domainTypeGen: Gen[String] = Gen.oneOf(Seq("com", "es", "org"))
+    def pathGen: Gen[String] = for
+      numberOfFragments <- Gen.choose(1, 3)
+      path <- Gen.listOfN[String](numberOfFragments, textGen()).map(_.mkString("/"))
+    yield path
     for
       http <- httpTypeGen
       domain <- domainGen
       domainType <- domainTypeGen
       path <- pathGen
     yield http + "//" + domain + "." + domainType + "/" + path
+
+  private[this] def textGen(minLength: Int = 3, maxLength: Int = 10): Gen[String] = for
+    length <- Gen.choose(minLength, maxLength)
+    text <- Gen.listOfN[Char](length, Gen.alphaNumChar).map(_.mkString)
+  yield text
+
+  sealed trait Fragment
+
+  object Fragment:
+    final case class LinkFragment(link: Link) extends Fragment
+    final case class TextFragment(text: String) extends Fragment
