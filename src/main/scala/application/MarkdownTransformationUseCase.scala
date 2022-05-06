148db2fccec3017303418472429fed89e2d082be
Erik Torres <erik.torres@lifullconnect.com>
2022-05-06 08:23:12 +0200

    :fire: Remove dead code

--- src/main/scala/application/MarkdownTransformationUseCase.scala
+++ src/main/scala/application/MarkdownTransformationUseCase.scala
@@ -5,10 +5,7 @@ import application.MarkdownTransformationUseCase.linkPattern
 import model.{Footnote, *}
 
 import cats.effect.IO
-import cats.implicits.*
-import fs2.{Chunk, Stream}
-
-import scala.collection.immutable.List
+import fs2.Stream
 
 final class MarkdownTransformationUseCase(
     markdownReader: MarkdownReader,
@@ -35,26 +32,6 @@ final class MarkdownTransformationUseCase(
       ),
     )
     .map(Line(_))
-
-  private[this] def transform2(line: Line) =
-    Stream
-      .unfoldChunkEval[IO, Option[String], Line](Some(line.value)) {
-        case Some(text) =>
-          (text match
-            case linkPattern(text, url) => Some(Link(LinkText(text), LinkUrl(url)))
-            case _ => Option.empty
-          ).fold(IO.delay(Some((Chunk(Line(text)), Option.empty)))) { link =>
-            for
-              reference <- footnotesRepository.save(link)
-              transformedText = linkPattern.replaceFirstIn(
-                text,
-                s"${link.text} [^${reference.value}]",
-              )
-            yield Some((Chunk(Line(transformedText)), Some(transformedText)))
-          }
-        case None => IO.none
-      }
-      .lastOr(Line.empty)
 
   private[this] def format(footnotes: IO[List[Footnote]]) = Stream
     .evals(footnotes.map(_.sorted))
